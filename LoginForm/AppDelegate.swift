89da47e63f7d87218e702137526f06740bdf5659
fqqName <bogpezd@gmail.com>
2022-10-31 21:33:57 +0300

    create project

---
+++ LoginForm/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  LoginForm
+//
+//  Created by Mikita Piatrushka on 31.10.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.green
+        
+        let screenVC = MainViewController()
+        
+        window?.rootViewController = screenVC
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
---
+++ LoginForm/Model/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  LoginForm
+//
+//  Created by Mikita Piatrushka on 31.10.22.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+
+    }
+ 
+
+}
